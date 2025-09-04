cc32a0f4a32b67b3eef5ee4cc9683943a07cf99a
hslee15 <lhs01019@kbu.ac.kr>
2025-09-04 21:39:38 +0900

    0904

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Hero from "./sections/Hero"
-import Brand from "./sections/Brand"
 import Cta from "./sections/Cta"
 import "./styles/main.scss"
 import TopBanner from "./components/TopBanner"
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import TopBtn from "./components/TopBtn"
 import Hello from "./sections/Hello"
 import Collection from "./sections/Collection"
@@ -15,30 +14,31 @@ import Instargram from "./sections/Instargram"
 
 function App() {
 
-  const [topBanner,setTopBanner]=useState("")
+  const [topBanner, setTopBanner] = useState("")
 
-  const [isScrolled,setIsScrolled]=useState(false)
+  const [isScrolled, setIsScrolled] = useState(false)
 
-  useEffect(()=>{
-    const handleScroll=()=>{
-      const scrollTop=window.scrollY
 
-      setIsScrolled(scrollTop>0)
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+
+      setIsScrolled(scrollTop > 0)
     }
-    window.addEventListener('scroll',handleScroll)
-    return ()=>window.removeEventListener('scroll',handleScroll)
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
 
   })
 
 
-  const upTopBanner =()=>{
+  const upTopBanner = () => {
     setTopBanner("up")
   }
 
   return (
-    <div className={`app-container  ${topBanner} ${isScrolled? "scrolled":""}`}>
-      <TopBtn/>
-      <TopBanner onClick={upTopBanner}/>
+    <div className={`app-container  ${topBanner} ${isScrolled ? "scrolled" : ""}`}>
+      <TopBtn />
+      <TopBanner onClick={upTopBanner} />
       <Header />
       <main>
         <section id="Hero" className="Section">
@@ -53,16 +53,13 @@ function App() {
         <section id="Collection" className="Section">
           <Collection />
         </section>
-        <section id="Brand" className="Section">
-          <Brand />
-        </section>
         <section id="Skincare" className="Section">
           <Skincare/>
         </section>
         <section id="Instar" className="Section">
           <Instargram/>
         </section>
-        
+
       </main>
       <Footer />
     </div>
--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import "../styles/sections/Instargram.scss"
 
 const Instargram = () => {
   return (
-    <div>Instargram</div>
+    <div className='inner instar-inner'>
+      <div className="t-wrap">
+        <h2 className='tit'>
+          Instagram
+        </h2>
+        <p>
+          @tocoba_official
+        </p>
+      </div>
+      <ul className="instar-lst">
+        {Images.map((img, idx)=>(
+        <li key={idx}>
+          <a href={img.link}>
+            
+          </a>
+        </li>
+          
+        ))}
+      </ul>
+    </div>
   )
 }
 

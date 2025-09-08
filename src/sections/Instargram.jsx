0ee26d5cdca199cf1ee4495911640c7cde6e4d75
hslee15 <lhs01019@kbu.ac.kr>
2025-09-08 21:45:01 +0900

    ii

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import "../styles/sections/instargram.scss"
 import Images from "../util/instar"
+import "../styles/sections/instagram.scss"
 
 const Instargram = () => {
   return (
